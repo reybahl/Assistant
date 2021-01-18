98999765141b521c19bf7ac8adaecdefd02d3674
ReyBahl <bahl.reyansh@gmail.com>
2021-01-18 11:51:23 -0500

    Changed naive bayes to SVM

--- intentclassification/intent_classification.py
+++ intentclassification/intent_classification.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
 from io import StringIO
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -13,12 +14,16 @@ class IntentClassifier:
         self.train()
 
     def train(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.data['text'], self.data['intent'], random_state = 0)
+        X_train, y_train= self.data['text'], self.data['intent']
         self.count_vect = CountVectorizer()
         X_train_counts = self.count_vect.fit_transform(X_train)
         tfidf_transformer = TfidfTransformer()
         X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-        self.clf = MultinomialNB().fit(X_train_tfidf, y_train)
+        # self.clf = MultinomialNB().fit(X_train_tfidf, y_train)
+        self.svm = LinearSVC().fit(X_train_tfidf, y_train)
     
     def predict(self, text):
-        return self.clf.predict(self.count_vect.transform([text]))[0]
+        return self.svm.predict(self.count_vect.transform([text]))[0]
+
+cl = IntentClassifier()
+print(cl.predict("no one could be better than you"))
