8f0defbca1ca31e7623629903d7a20f7d42493f1
reybahl <bahl.reyansh@gmail.com>
2022-07-17 13:55:24 -0400

    code cleanup intent classification

--- intentclassification/intent_classification.py
+++ intentclassification/intent_classification.py
@@ -4,21 +4,20 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.svm import LinearSVC
 
 class IntentClassifier:
-    def __init__(self):
-        self.data = pd.read_csv('intentclassification/data.csv')
-        self.train()
+	def __init__(self):
+		self.data = pd.read_csv('intentclassification/data.csv')
+		self.train()
+		# train upon instantiation
 
-    def train(self):
-        X_train, y_train= self.data['text'], self.data['intent']
-        self.count_vect = CountVectorizer()
-        X_train_counts = self.count_vect.fit_transform(X_train)
-        tfidf_transformer = TfidfTransformer()
-        X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-        # self.clf = MultinomialNB().fit(X_train_tfidf, y_train)
-        self.svm = LinearSVC().fit(X_train_tfidf, y_train)
-    
-    def predict(self, text):
-        return self.svm.predict(self.count_vect.transform([text]))[0]
+	def train(self):
+		X_train, y_train = self.data['text'], self.data['intent']
+		self.count_vect = CountVectorizer()
+		X_train_counts = self.count_vect.fit_transform(X_train)
+		tfidf_transformer = TfidfTransformer()
+		X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+		self.svm = LinearSVC().fit(X_train_tfidf, y_train)
+	
+	def predict(self, text):
+		return self.svm.predict(self.count_vect.transform([text]))[0]
 
 intent_classifier = IntentClassifier()
-print(intent_classifier.predict("Hello, how are you?"))
