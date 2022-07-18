505e11162c4abbe8f6b1895ee24702a90caf95d2
reybahl <bahl.reyansh@gmail.com>
2022-07-18 11:19:44 -0400

    formatting changes intent_classification.py

--- intentclassification/intent_classification.py
+++ intentclassification/intent_classification.py
@@ -1,13 +1,14 @@
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.svm import LinearSVC
+
 
 class IntentClassifier:
 	def __init__(self):
 		self.data = pd.read_csv('intentclassification/data.csv')
-		self.train()
 		# train upon instantiation
+		self.train()
+
 
 	def train(self):
 		X_train, y_train = self.data['text'], self.data['intent']
@@ -16,8 +17,10 @@ class IntentClassifier:
 		tfidf_transformer = TfidfTransformer()
 		X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 		self.svm = LinearSVC().fit(X_train_tfidf, y_train)
-	
+
+
 	def predict(self, text):
 		return self.svm.predict(self.count_vect.transform([text]))[0]
+
 
 intent_classifier = IntentClassifier()
